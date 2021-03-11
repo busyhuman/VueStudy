79464f4123e1482a87c04d51de850d450efe434a
guseorkd <guseorkd@gmail.com>
2021-03-11 18:45:11 +0900

    5_refactoring2

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,13 +1,23 @@
 <template>
   <div>
-    <p>name : {{ userInfo.id }}</p>
+    <!-- :info="userInfo"는 propsData로 info에 userInfo(state에 user 데이터를 갖고 있는)를 넣어주겠다는 것이다. 
+    그러면 UnserProfile에서 info를 받아 화면에 뿌려주는 것이다. -->
+    <user-profile :info="userInfo"></user-profile>
+
+    <!-- <p>name : {{ userInfo.id }}</p>
     <p>karma : {{ userInfo.karma }}</p>
-    <p>created : {{ userInfo.created }}</p>
+    <p>created : {{ userInfo.created }}</p> -->
   </div>
 </template>
 
 <script>
+import UserProfile from '../components/UserProfile.vue'
+
 export default {
+  components: {
+    UserProfile,
+  },
+
   computed: {
     userInfo() {
       return this.$store.state.user
@@ -15,7 +25,6 @@ export default {
   },
   created() {
     const userName = this.$route.params.id
-    // axios.get(`http://api.hnpwa.com/v0/user/${userName}.json`);
     this.$store.dispatch('FETCH_USER', userName)
   },
 }
