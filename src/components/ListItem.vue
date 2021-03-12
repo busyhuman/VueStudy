f2dd6cef9e88ca599ca506f3f698f833f46663cf
guseorkd <guseorkd@gmail.com>
2021-03-12 18:34:15 +0900

    6_refactoring3

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -43,20 +43,7 @@
 
 <script>
 export default {
-  created() {
-    // this.$store.dispatch("FETCH_NEWS")
-    // console.log(this.$route.path === "/news")
-    const name = this.$route.name
-    if (name === 'news') {
-      this.$store.dispatch('FETCH_NEWS')
-    } else if (name === 'ask') {
-      this.$store.dispatch('FETCH_ASK')
-    } else if (name === 'jobs') {
-      this.$store.dispatch('FETCH_JOBS')
-    }
-  },
-
-  computed: {
+   computed: {
     listItems: function() {
       const name = this.$route.name
       if (name === 'news') {
