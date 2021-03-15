606ea6151bea6297911c0adeafcd072332ca0aa2
guseorkd <guseorkd@gmail.com>
2021-03-15 18:50:17 +0900

    7_refactoring4

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,17 +1,29 @@
 import { fetchAskList, fetchUserInfo, fetchJobsList, fetchNewsList, fetchCommentItem, fetchList } from '../api/index.js';
 
 export default {
-    FETCH_NEWS(context) {
-        return fetchNewsList()
-            .then(response => {
-                // Mutations 호출
-                context.commit('SET_NEWS', response.data);
-                return response;
-            })
-            .catch(error => {
-                console.log(error);
-            });
+    // promise
+    // FETCH_NEWS(context) {
+    //     return fetchNewsList()
+    //         .then(response => {
+    //             // Mutations 호출
+    //             context.commit('SET_NEWS', response.data);
+    //             return response;
+    //         })
+    //         .catch(error => {
+    //             console.log(error);
+    //         });
+    // },
+
+    // async
+    async FETCH_NEWS(context) {
+        const response = await fetchNewsList();
+        context.commit('SET_NEWS', response.data);
+
+        // 결과 값을 반환해줘야 FETCH_NEWS를 실행하고나서 그 다음에 처리를 이어서 할 수 있다. (체이닝)
+        return response;
     },
+
+
 
     FETCH_JOBS({ commit }) {
         return fetchJobsList()
